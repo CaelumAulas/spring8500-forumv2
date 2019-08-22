1e887776d94f20085aeb81aace23096dd9cb7e20
Ana Losnak <ana.losnak@caelum.com.br>
2019-08-22 10:18:36 -0300

    ex 9.3 - salvando um tópico

--- src/main/java/br/com/alura/forum/controller/TopicController.java
+++ src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +10,23 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.alura.forum.controller.dto.input.NewTopicInputDto;
 import br.com.alura.forum.controller.dto.input.TopicSearchInputDto;
 import br.com.alura.forum.controller.dto.output.TopicBriefOutputDto;
 import br.com.alura.forum.controller.dto.output.TopicDashboardItemOutputDto;
+import br.com.alura.forum.controller.dto.output.TopicOutputDto;
+import br.com.alura.forum.model.User;
 import br.com.alura.forum.model.topic.domain.Topic;
+import br.com.alura.forum.repository.CourseRepository;
 import br.com.alura.forum.repository.TopicRepository;
 import br.com.alura.forum.service.DashboardDataProcessingService;
 import br.com.alura.forum.vo.CategoriesAndTheirStatisticsData;
@@ -30,6 +40,8 @@ public class TopicController {
 	
 	@Autowired
 	private DashboardDataProcessingService dashboardDataProcessingService;
+	@Autowired
+	private CourseRepository courseRepository;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public Page<TopicBriefOutputDto> listTopics(TopicSearchInputDto topicSearch,
@@ -48,4 +60,32 @@ public class TopicController {
     		return TopicDashboardItemOutputDto.listFromCategories(categoriesStatisticsData);
     		
     }
+    
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+    			produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> salva(@RequestBody NewTopicInputDto inputDto,
+    		@AuthenticationPrincipal User loggedUser) {
+		Topic topic = inputDto.build(courseRepository, loggedUser);
+		topicRepository.save(topic);
+		
+		URI location = UriComponentsBuilder.fromPath("/api/topics/{id}")
+				.buildAndExpand(topic.getId()).toUri();
+		TopicOutputDto topicOutputDto = new TopicOutputDto(topic);
+		return ResponseEntity.created(location).body(topicOutputDto);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
---
+++ src/main/java/br/com/alura/forum/controller/dto/input/NewTopicInputDto.java
@@ -0,0 +1,37 @@
+package br.com.alura.forum.controller.dto.input;
+
+import br.com.alura.forum.model.Course;
+import br.com.alura.forum.model.User;
+import br.com.alura.forum.model.topic.domain.Topic;
+import br.com.alura.forum.repository.CourseRepository;
+
+public class NewTopicInputDto {
+	private String shortDescription;
+    private String content;
+    private String courseName;
+    
+	public String getShortDescription() {
+		return shortDescription;
+	}
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	public Topic build(CourseRepository courseRepository, User loggedUser) {
+		Course course = courseRepository.findByName(courseName);
+		return new Topic(shortDescription, content, loggedUser, course); 
+	}
+
+
+}
